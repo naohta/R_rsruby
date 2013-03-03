ff92dedbd2b65f6feeee693fd798240509fade87
Naohiro OHTA <naohta@gmail.com>
2013-03-03 21:50:45 +0900

    Work! Draw graph and save as png!

--- my1st.rb
+++ my1st.rb
@@ -1,5 +1,11 @@
 require 'rsruby'
 
-r = RSRuby.instance
-dist = r.rnorm(10,0,1)
-puts dist.join(",")
+@@r = RSRuby.instance
+@@r.png("sample.png")
+@@r.plot([1,2,3,4,5],[10,5,2,5,10],
+  xlab:"something",
+  ylab:"yap",
+  type:"b",
+  main:"Simple graph",
+  col:"blue")
+@@r.eval_R("dev.off()")
